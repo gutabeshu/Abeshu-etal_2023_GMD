4bbcbe5735b72305d6ddc4edf670aa6e909f7e05
Guta W. Abeshu <40370576+gutabeshu@users.noreply.github.com>
2022-06-23 21:04:43 -0500

    Add files via upload

---
+++ workflow/Basin43.py
@@ -0,0 +1,26 @@
+
+import os
+
+from xanthos import Xanthos
+
+
+def run(ini):
+
+    # instantiate model
+    xth = Xanthos(ini)
+
+    # run intended configuration
+    xth.execute()
+
+    return xth
+
+
+if __name__ == "__main__":
+
+    # full path to parameterized config file
+    ini = os.path.join('/project/hli/gabeshu/Guta_Working/BasinsFile/xanthos43/pm_abcd_mrtm43.ini')
+
+    # run the model
+    xth = run(ini)
+
+    del xth
